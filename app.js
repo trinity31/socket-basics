eb562ef1c75fc145a89f5584d25577b1d4b67d7b
Trinity Song <kyeongyeon.song@gmail.com>
2016-09-25 13:31:53 +0900

    Added front-end form to submit message

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ socket.on('message', function(message) {
 	console.log("New Message!");
 	console.log(message.text);
 });
+
+// Handles submitting a new message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+	event.preventDefault(); //prevent old-fashioned event handling
+	socket.emit('message', {
+		text: $form.find('input[name=message]').val(); //find any input tag which has 'message' in 'name' attribute
+	});
+});
