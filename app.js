8895230cb10cbf8b2fc6a46b62b240ef46189cac
Trinity Song <kyeongyeon.song@gmail.com>
2016-09-25 15:53:45 +0900

    Add messages to browser UI

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ socket.on('connect', function() {
 socket.on('message', function(message) {
 	console.log("New Message!");
 	console.log(message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 });
 
 // Handles submitting a new message
